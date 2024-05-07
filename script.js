ec4c953b269b2d17a77f31bd7dd51f5e0402fd61
WilsonEHerrera <wilsondejespinosaherrera@gmail.com>
2024-05-06 21:09:59 -0500

    Se agregaron nuevas vistas y se me modifico un poco las anteriorires

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 $(function(){
     $("#navbar-contenedor").load("navbar.html");
 });
+
+$(function(){
+    $("#navbar-contenedorTutor").load("navbarTutor.html");
+});
+
+$(function(){
+    $("#navbar-contenedorAdmin").load("navbarAdmin.html");
+});
